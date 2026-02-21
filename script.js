0674cb1798df9c273deee86254b6fc4674ac43f7
jihoon2011 <pa2410620@gsuite.gen.go.kr>
2026-02-22 01:45:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const bg = document.querySelector('.bg');
 
 document.addEventListener('mousemove', (e) => {
-    const { innerWidth, innerHeight } = window;
+    const x = e.clientX - window.innerWidth / 2;
+    const y = e.clientY - window.innerHeight / 2;
 
-    const x = e.clientX - innerWidth / 2;
-    const y = e.clientY - innerHeight / 2;
-
-    const rotateX = (-y / innerHeight) * 10; // 위아래
-    const rotateY = (x / innerWidth) * 10;   // 좌우
+    const rotateX = (-y / window.innerHeight) * 8; // 기울기 조금 줄임
+    const rotateY = (x / window.innerWidth) * 8;
 
     bg.style.transform = `
         perspective(1000px)
         rotateX(${rotateX}deg)
         rotateY(${rotateY}deg)
     `;
+
+    bg.style.filter =
+        `drop-shadow(${ -rotateY * 2 }px ${ rotateX * 2 }px 50px rgba(0,0,0,0.7))`;
 });
 
 document.addEventListener('mouseleave', () => {
@@ -22,4 +23,7 @@ document.addEventListener('mouseleave', () => {
         rotateX(0deg)
         rotateY(0deg)
     `;
+
+    bg.style.filter =
+        `drop-shadow(0 20px 40px rgba(0,0,0,0.6))`;
 });
