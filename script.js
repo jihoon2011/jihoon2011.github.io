1e251ca6a3f1565b090dadbae332d12612e7ebcf
jihoon2011 <pa2410620@gsuite.gen.go.kr>
2026-02-22 01:57:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ document.addEventListener('mousemove', (e) => {
     const x = e.clientX - window.innerWidth / 2;
     const y = e.clientY - window.innerHeight / 2;
 
-    const rotateX = (-y / window.innerHeight) * 8;
-    const rotateY = (x / window.innerWidth) * 8;
+    const rotateX = (-y / window.innerHeight) * 6; // 각도도 살짝 줄임
+    const rotateY = (x / window.innerWidth) * 6;
 
     bg.style.transform = `
         perspective(1000px)
+        scale(0.85)
         rotateX(${rotateX}deg)
         rotateY(${rotateY}deg)
     `;
@@ -17,10 +18,10 @@ document.addEventListener('mousemove', (e) => {
         `drop-shadow(${ -rotateY * 2 }px ${ rotateX * 2 }px 50px rgba(0,0,0,0.7))`;
 });
 
-/* 🔥 여기만 수정 */
 bg.addEventListener('mouseleave', () => {
     bg.style.transform = `
         perspective(1000px)
+        scale(0.85)
         rotateX(0deg)
         rotateY(0deg)
     `;
@@ -29,7 +30,6 @@ bg.addEventListener('mouseleave', () => {
         `drop-shadow(0 20px 40px rgba(0,0,0,0.6))`;
 });
 
-/* 클릭 이동 */
 bg.addEventListener('click', () => {
     window.location.href = "https://chzzk.naver.com/64d76089fba26b180d9c9e48a32600d9";
 });
